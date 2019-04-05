36f5247bb2b9e19eff483f695d8f6c27c2b4695b
Kim Cantrell <kimquat8@gmail.com>
2019-04-04 22:40:51 -0700

    importing project

---
+++ Passcode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+
+            while (code != "secret")
+            {
+
+
+                Console.WriteLine("What is the passcode?");
+                code = Console.ReadLine();
+
+                 if (code != "secret")
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+            Console.WriteLine("You are successfully authenticated!");
+        }
+    }
+}
+
