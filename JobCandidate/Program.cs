866f5525170d53c17634c7b65c192fed1944e077
Kim Cantrell <kimquat8@gmail.com>
2019-04-08 13:06:39 -0700

    importing

---
+++ JobCandidate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience ya got?");
+
+            var years = int.Parse(Console.ReadLine());
+
+            switch(years)
+            {
+                case 0:
+                    Console.WriteLine("Inexperienced.");
+                    break;
+                case 1:
+                    Console.WriteLine("Junior.");
+                    break;
+                case 2:
+                    Console.WriteLine("Intermediate.");
+                    break;
+                case 3:
+                    Console.WriteLine("Advanced.");
+                    break;
+                default:
+                    Console.WriteLine("Senior.");
+                    break;
+
+            }
+
+
+        }
+    }
+}
