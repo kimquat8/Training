9643bb0d2a4aed835142bb92e61fe189f32a6854
Kim Cantrell <kimquat8@gmail.com>
2019-04-08 11:48:39 -0700

    commit staged

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StudentGrades = new int[] { 80, 77, 65, 100, 98, 42, 90, 12, 55, 96 };
+
+            foreach (var StudentGrade in StudentGrades)
+            {
+                Console.WriteLine(StudentGrade);
+            }
+            
+        }
+    }
+}
